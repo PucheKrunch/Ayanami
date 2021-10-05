b17871acd5fd0b1e74e8a3d74484f38085c65206
PucheKrunch <santiago.benavides@alumnos.udg.mx>
2021-10-04 19:32:27 -0500

    User register and login authentication added

--- store/webstore/views.py
+++ store/webstore/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
@@ -8,14 +12,40 @@ def home(request):
     return render(request,'home.html',context)
 
 def cart(request,pk):
-    #Hola soy un comentario
     client = Client.objects.get(pk=pk)
 
     context = {'client':client}
     return render(request,'cart.html',context)
 
-def login(request):
-    return render(request,'login.html',{'data':1})
+def user_login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            try:
+                login(request, user)
+                return redirect('home')
+            except:
+                messages.success(request,"Ha ocurrido un error, intenta de nuevo")
+                return render(request,'login.html',context)
+        else:
+            messages.success(request,"Usuario no registrado")
+            return render(request,'login.html',context)
+
+    return render(request,'login.html',context)
 
 def register(request):
-    return render(request,'register.html',{'data':1})
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,"Cuenta creada para exitosamente para " + user)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request,'register.html',context)
